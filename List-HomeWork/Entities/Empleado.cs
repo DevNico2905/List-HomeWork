8400555354c4b3eda6aeb71aa071bbdbdb31e1b6
DevNico2905 <nicobernal2905@gmail.com>
2026-03-09 19:29:23 -0500

    Update: All Empleado entity properties

--- List-HomeWork/Entities/Empleado.cs
+++ List-HomeWork/Entities/Empleado.cs
@@ -4,5 +4,9 @@ namespace List_HomeWork.Entities;
 
 public class Empleado
 {
-
+    public int Id { get; set; }
+    public string Nombre { get; set; } = String.Empty;
+    public string Departamento { get; set; } = String.Empty;
+    public double Salario { get; set; }
+    public bool Activo { get; set; }
 }
